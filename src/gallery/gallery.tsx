b60c0cbfa93f89a257343ac9d01c31a6e68d8f38
designus <info@designus.lt>
2022-01-20 20:30:49 +0100

    remove files

--- src/gallery/gallery.tsx
+++ src/gallery/gallery.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { ErrorBoundary } from '../errorBoundary';
 import { ImageSource } from '../types';
 import { ImageList, Props } from '../imageList';
+import './gallery.css';
 
 export function Gallery<T extends ImageSource>(props: Props<T>) {
   return (
